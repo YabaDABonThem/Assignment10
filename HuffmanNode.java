36382b057ee988b2af7d6ce2764a2cb9c88654da
Yabadaba <allenbao01@gmail.com>
2022-02-23 21:22:50 -0800

    step 2 done bruh

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -6,14 +6,21 @@
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class HuffmanNode implements Comparable<HuffmanNode> {
     public int frequency;
     public char character;
     public HuffmanNode left;
     public HuffmanNode right;
+
+    public HuffmanNode(char character, int frequency) {
+        this.frequency = frequency;
+        this.character = character;
+    }
 
     public static Map<Character, Integer> getCounts(FileInputStream input) throws IOException {
         Map<Character, Integer> characterCountMap = new HashMap<Character, Integer>();
@@ -30,15 +37,15 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
         return characterCountMap;
     }
 
-
-
-    public boolean isLeaf() {
-        return false;
-    }
-
     // Code so program doesn't break (need to implement compareTo method)
-    @Override
+    // Method to "sort" items in the PriorityQueue
     public int compareTo(HuffmanNode o) {
-        return 0;
+        if (frequency < o.frequency) {
+            return -1;
+        } else if (frequency == o.frequency) {
+            return 0;
+        }
+        return 1;
     }
+
 }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -5,11 +5,33 @@
 // HuffmanTree class for Huffman Coding Assignment (Assignment 10)
 
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class HuffmanTree {
     public HuffmanTree(Map<Character, Integer> counts) { // Constructor
 
+    }
+
+    public PriorityQueue<HuffmanNode> sortItems(Map<Character, Integer> characterCountMap) {
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        Collection<Character> characters = characterCountMap.keySet();
+        for (Character key : characters) {
+            priorityQueue.add(new HuffmanNode(key, characterCountMap.get(key)));
+        }
+        System.out.println(priorityQueue);
+        return priorityQueue;
+    }
+
+    public HuffmanTree priorityQueueToTree(PriorityQueue<HuffmanNode> priorityQueue) {
+        for (HuffmanNode i : priorityQueue) {
+
+        }
+    }
+
+    public boolean isLeaf() {
+        return false;
     }
 
     public StringBuilder compress(InputStream inputFile) { // inputFile is a text file
